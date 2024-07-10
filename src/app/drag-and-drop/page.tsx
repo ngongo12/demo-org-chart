cd447867f64f5d07796c628053f09f0c23ab53a8
Ngô Quốc Vinh <71309432+ngongo12@users.noreply.github.com>
2024-07-10 17:27:10 +0700

    update

---
+++ src/app/drag-and-drop/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import DragDropChart from "@/common/drag-drop-chart/drag-drop-chart";
+const page = () => {
+  return <DragDropChart />;
+};
+
+export default page;
---
+++ src/app/edit-chart/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import EditChart from "@/common/edit-chart/edit-chart";
+const index = () => {
+  return <EditChart />;
+};
+
+export default index;
